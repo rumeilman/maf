a377337e3b32f3bae5bebfcb8210c536a5306550
rumeilman <maisyaarrum51@gmail.com>
2025-01-13 21:32:02 +0700

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -24,10 +24,10 @@ about:{ id:'about', css:'col-1-3', data:[
 },
 
 stats:{ id:'stats', css:'col-1-3',data:[
-{id:1,nama:'CLIENTS',isi:10,icon:'eye'},
-{id:1,nama:'PROJECTS',isi:10,icon:'camera'},
-{id:1,nama:'POSITIVE FEEDBACKS',isi:10,icon:'geo'},
-{id:1,nama:'WEBSITE LAUNCH',isi:10,icon:'pen'},
+{id:1,nama:'CLIENTS',isi:50,icon:'eye'},
+{id:1,nama:'PROJECTS',isi:98,icon:'camera'},
+{id:1,nama:'POSITIVE FEEDBACKS',isi:80,icon:'geo'},
+{id:1,nama:'WEBSITE LAUNCH',isi:34,icon:'pen'},
 ],
 },
 
